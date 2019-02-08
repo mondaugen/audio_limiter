0fe99690960c53111806a7b096637db7766e7676
Nicholas Esterer <nicholas.esterer@gmail.com>
2019-02-08 08:46:06 -0500

    Tranfer function modelling works ok

--- exp_sum_optimize2.py
+++ exp_sum_optimize2.py
@@ -2,15 +2,33 @@
 # sense
 # Use log sum of exp for better precision
 
+# Conclusions:
+# I think no solution exists for many configurations of n_p and n_d so this
+# cannot hope to find coefficients giving these values
+# Really, as long as the
+# peak is close to n_p and the n_p is not so different, then probably the
+# transfer functions will sound ok.  But the problem is that the transfer
+# function still needs to be inspected by hand BUT probably this the best
+# transfer function you will get only using a filter of order 2, so you should
+# just accept the answer the computer gives Also the solver still seems to try
+# infeasible points, but still succeeds when there is a solution, so this is
+# weird (bug?)
+
 import matplotlib.pyplot as plt
 import numpy as np
 import scipy
+import pdb
 from scipy import optimize
+import warnings
+#warnings.filterwarnings('error')
 
 def log_sum_exp(x,b):
     # compute log(sum(exp(x))) but with improved precision for large x
     a=np.max(x)
-    return a+np.log(np.sum(b*np.exp(x-a)))
+    try:
+        return a+np.log(np.sum(b*np.exp(x-a)))
+    except RuntimeWarning:
+        pdb.set_trace()
 
 def obj_fun(x,n_p,n_d,max_w):
     # n_p is the sample index of the peak
@@ -19,12 +37,29 @@ def obj_fun(x,n_p,n_d,max_w):
     b=x
     u=0
     # f'(n_p) = 0
-    u+=max_w*(np.log(-b[0])-np.log(-b[1])-(b[1]-b[0])*n_p)**2
+    try:
+        u+=max_w*(np.log(-b[0])-np.log(-b[1])-(b[1]-b[0])*n_p)**2
+    except RuntimeWarning:
+        pdb.set_trace()
     # f(n_d) = f(n_p)*1e-3
     u+=(log_sum_exp(b*n_d,np.array([1,-1]))
         -log_sum_exp(b*n_p,np.array([1,-1]))
         -np.log(1e-3))**2
     return np.array([u])
+
+def grid_search(n_p,n_d,max_w,N_points=10):
+    res=[]
+    arg_res=[]
+    b_min=-0.1
+    for b0 in b_min/np.power(2,1+np.arange(N_points)):
+        for b1 in (b_min-b0)/np.power(2,1+np.arange(N_points))+b0:
+            #print([b0,b1])
+            b=np.array([b0,b1])
+            res.append(obj_fun(b,n_p,n_d,max_w)[0])
+            arg_res.append(b)
+        #print()
+    min_res_i=np.argmin(res)
+    return arg_res[min_res_i],res[min_res_i]
 
 def jac_obj_fun(x,n_p,n_d,max_w):
     b=x
@@ -42,13 +77,23 @@ def jac_obj_fun(x,n_p,n_d,max_w):
 
 def plot_result(x,n_p,n_d):
     b=x
-    n=np.arange(n_d)
+    n=np.arange(n_d+1)
     y=np.exp(b[0]*n)-np.exp(b[1]*n)
-    plt.plot(n,y)
+    y_max=np.max(y)
+    y_arg_max=np.argmax(y)
+    y/=y_max
+    print("Filter coeffs: " + str(np.exp(b)))
+    print("Value at n_p: %f" % (20*np.log10(y[n_p]),))
+    print("Value at n_d: %f" % (20*np.log10(y[n_d]),))
+    print("Location of peak: %d" % (y_arg_max,))
+    print("Desired peak: %d" % (n_p,))
+    print("n_d: %d" % (n_d,))
+    plt.plot(n,20*np.log10(y))
 
 M=2
-n_p=100
-n_d=1000
+n_p=300
+n_d=64000
+max_w=1
 
 #constr=optimize.LinearConstraint(
 #np.array([
@@ -69,18 +114,30 @@ def constr(x):
     # first value means b0 never greater than it (should be non-positive)
     # second value means b1 never greater than it (should be non-positive)
     # last value means b1-b0 less than it
-    return -1*A.dot(x)+np.array([-1e-3,-1e-3,-1e-4])
+    return -1*A.dot(x)+np.array([-1e-6,-1e-6,-1e-8])
 
-b0=-0.01*np.random.ranf()
-b1=-0.01*np.random.ranf()+b0
+def jac_constr(x):
+    A=np.array([
+        [1,0],
+        [0,1],
+        [-1,1]
+    ])
+    return -1*A
+
+# Find starting point using grid search
+b,r=grid_search(n_p,n_d,max_w,N_points=10)
+print("starting b "+str(b))
+print("starting cost "+str(r))
 result=optimize.minimize(
 obj_fun,
-np.array([b0,b1]),
-args=(n_p,n_d,100),
+b,
+args=(n_p,n_d,max_w),
 jac=jac_obj_fun,
+method='SLSQP',
 constraints=[dict(
 type='ineq',
-fun=constr)])
+fun=constr,
+jac=jac_constr)])
 
 print("Success: %s" % (str(result.success),))
 print(result.x)
