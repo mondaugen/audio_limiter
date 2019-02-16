0b965e387d9813c4c3b249abfbd72c841220ca9d
Nicholas Esterer <nicholas.esterer@gmail.com>
2019-02-15 19:38:52 -0500

    A lot of limiter code
    Not finished.

--- limiter_ir_af.c
+++ limiter_ir_af.c
@@ -1,9 +1,217 @@
-/* A limiter whose attenuation function's decay is described by an IIR filter.
+/*
+A limiter whose attenuation function's decay is described by an IIR filter.
 */
 
-/* A filter whose IR is stored with it. */
+#ifndef MAX
+#define MAX(x,y) (((x)>(y))?(x):(y))
+#endif
+
+static unsigned int
+argmax(float *array, unsigned int len)
+{
+    float max = array[0];
+    unsigned int maxi = 0;
+                 n = 1;
+    while (n < len) {
+        if (array[n] > max) {
+            maxi = n;
+            max = array[n];
+        }
+    }
+    return maxi;
+}
+
 struct filter_w_ir {
-    float *b;
-    float *a;
+    struct filter_w_ir_init config;
+    /* The index of the maxmimum value in the stored IR */
+    unsigned int ir_argmax;
+    unsigned int filter_order;
+    struct filter_w_ir_filter_imp filter;
 };
 
+void
+filter_w_ir_free(struct filter_w_ir *f)
+{
+    if (!f) { return; }
+    if (f->filter.free_filter_aux) {
+        f->filter.free_filter_aux(f->filter.filter_aux);
+    }
+    free(f);
+}
+
+static int
+filter_w_ir_arg_chk(struct filter_w_ir_init * i)
+{
+    if (i->block_size < 1) { return -1; }
+    if ((i->len_b > 0) && (i->b == NULL)) { return -2; }
+    if ((i->len_a > 0) && (i->a == NULL)) { return -3; }
+    if ((i->len_ir > 0) && (i->ir == NULL)) { return -4; }
+    return 0;
+}
+
+struct filter_w_ir *
+filter_w_ir_new(struct filter_w_ir_init *fwirinit)
+{
+    if (filter_w_ir_arg_chk(fwirinit)) { return NULL; }
+    struct filter_w_ir *ret = calloc(1,sizeof(struct filter_w_ir));
+    if (!ret) { return NULL; }
+    ret->config = *fwirinit;
+    if (filter_w_ir_init_filter(&ret->filter,fwirinit)) {
+        free(ret);
+        return NULL;
+    }
+    ret->ir_argmax = argmax(fwirinit.ir,fwirinit.len_ir);
+    ret->filter_order = MAX(fwirinit->len_b,fwirinit->len_a);
+    return ret;
+}
+
+struct limiter_ir_af_init {
+    /* The implementation of the filter with the IR stored. */
+    struct filter_w_ir *fwir;
+    /* The number of samples that are computed each call to the
+    limiter_ir_af_tick function */
+    unsigned int block_size;
+    /* The threshold over which absolute signal values are limited */
+    float threshold;
+};
+
+struct limiter_ir_af {
+    struct limiter_ir_af_init config;
+    unsigned int ramp_up;
+    unsigned int lookahead_buf_size;
+    /* a buffer_size worth of zeros, used as the input to the attenuation
+    function's filter. */
+    float *zeros;
+    /* Where the filter's past_x values are stored so we can force the filter to
+    have them when it filters */
+    float *past_x;
+    /* Where the filter's past_y values are stored */
+    float *past_y;
+    struct float_buf *lookahead_buf;
+    struct float_buf *attenuation_buf;
+};
+
+static int
+limiter_ir_af_arg_chk(limiter_ir_af_init *i)
+{
+    if (i->fwir == NULL) { return -1;
+    if (i->threshold < 0) { return -2; }
+    if (i->fwir.ir_argmax < 1) { return -3; }
+    return 0;
+}
+
+void
+limiter_ir_af_free(struct limiter_ir_af *lia)
+{
+    if (!lia) { return; }
+    if (lia->lookahead_buf) { float_buf_free(lia->lookahead_buf); }
+    if (lia->attenuation_buf) { float_buf_free(lia->attenuation_buf); }
+    free(lia);
+}
+
+struct limiter_ir_af *
+limiter_ir_af_new(struct limiter_ir_af_init *i)
+{
+    if (limiter_ir_af_arg_chk(i)) { return NULL; }
+    struct limiter_ir_af *ret = NULL;
+    unsigned int ramp_up = i->fwir.ir_argmax,
+                 lookahead_buf_size = i->fwir.block_size + ramp_up; 
+    ret = calloc(1,sizeof(struct limiter_ir_af)+sizeof(float)*i->config.buffer_size);
+    if (!ret) { goto fail; }
+    ret->zeros = (float*)(ret+1);
+    ret->past_x = ret->zeros + i->config.fwir->filter_order;
+    ret->past_y = ret->past_x + i->config.fwir->filter_order;
+    ret->lookahead_buf = float_buf_new(lookahead_buf_size);
+    if (!ret->lookahead_buf) { goto fail; }
+    ret->attenuation_buf = float_buf_new(lookahead_buf_size);
+    if (!ret->attenuation_buf) { goto fail; }
+    ret->config = *i;
+    ret->ramp_up = ramp_up;
+    ret->lookahead_buf_size = lookahead_buf_size;
+    return ret;
+fail:
+    limiter_ir_af_free(ret);
+    return NULL;
+}
+
+static unsigned int inline
+lia_buffer_size(struct limiter_ir_af *lia)
+{
+    return lia->config.buffer_size;
+}
+
+static unsigned int inline
+lia_filter_order(struct limiter_ir_af *lia)
+{
+    return lia->config.fwir->filter_order;
+}
+
+struct fb_filter_region_aux {
+    struct filter_w_ir_filter_imp *f_imp;
+    float *input_vals;
+    float *past_x;
+    float *past_y;
+};
+
+static void
+fb_filter_region(
+    float *seg,
+    unsigned int len,
+    void *aux)
+{
+    struct fb_filter_region_aux *ffrb = aux;
+    (void)filter_w_ir_filter_imp_tick(
+        ffrb->f_imp,
+        ffrb->input_vals,
+        seg,
+        len,
+        ffrb->past_x,
+        ffrb->past_y);
+    ffrb->input_vals += len;
+}
+
+/*
+Limits x according to the configuration of lia.
+There will be an output delay of lia->ramp_up.
+x is processed in place.
+*/
+int
+limiter_ir_af_tick(struct limiter_ir_af *lia, float *x)
+{
+    /* Shift in samples from x */
+    float_buf_shift_in(lia->lookahead_buf,x,lia_buffer_size(lia))
+    /*
+    Shift in the attenuation function by computing the IR values based on the
+    last known values of the attenuation function
+    */
+    /*
+    it has this prototype 
+    int
+    float_buf_proc_region(
+     struct float_buf *fb,
+     unsigned int start,
+     unsigned int length,
+     void (*proc_fun)(float *seg, unsigned int len, void *aux),
+     void *aux);
+    */
+    memset(lia->past_x,0,sizeof(float)*lia_filter_order(lia));
+    float_buf_memcpy(
+        lia->attenuation_buf,
+        lia->ramp_up-lia_filter_order(lia),
+        lia->ramp_up,
+        lia->past_y);
+    struct fb_filter_region_aux filter_region_aux = {
+        .f_imp = lia->config.fwir,
+        .input_vals = lia->zeros,
+        .past_x = lia->past_x,
+        .past_y = lia->past_y,
+    };
+    float_buf_shift_in(lia->attenuation_buf,lia->zeros,lia_buffer_size(lia));
+    float_buf_process_region(
+        lia->attenuation_buf,
+        lia->ramp_up,
+        lia->lookahead_buf_size - lia->ramp_up,
+        fb_filter_region,
+        &filter_region_aux);
+
+} 
---
+++ limiter_ir_af.h
@@ -0,0 +1,57 @@
+#ifndef LIMITER_IR_AF_H
+#define LIMITER_IR_AF_H 
+
+/* A filter with a part of its IR stored with it. */
+struct filter_w_ir_init {
+    /* The feedforward coefficients */
+    float *b;
+    /* The number of feedforward coefficients */
+    unsigned int len_b;
+    /* The feedback coefficients, these are a1,a2,... a0 is assumed to be 1 */
+    float *a;
+    /* The number of feedback coefficients */
+    unsigned int len_a;
+    /* The precomputed IR */
+    float *ir;
+    /* The number of samples of the IR that have been computed */
+    unsigned int len_ir;
+};
+
+/* Auxilary structure for filter */
+struct filter_w_ir_filter_imp {
+    /* Stuff the filtering function might need (e.g., past filter states) */
+    void *filter_aux;
+    /* How to free this, can be NULL, in which case it is not called. */
+    void (*free_filter_aux)(void *);
+};
+
+/*
+Initialize the filtering routines.
+Returns non-zero on error, 0 otherwise.
+This needs to be defined somewhere in the application. It might be defined
+differently depending on the DSP library used.
+*/
+int
+filter_w_ir_init_filter(
+    struct filter_w_ir_filter *filter,
+    struct filter_w_ir_init *init);
+
+/*
+Filter x and put output in y. x and y should not overlap and should have
+length at least block_size.
+x_prev is {x[-M],x[-M+1],...,x[0]} where M is order of the filter.
+y_prev is similar.
+This needs to be defined somewhere and is what actually carries out the
+filtering.
+It needs to return non-zero on error.
+After returning, x_prev and y_prev should contain the most current filter state.
+*/
+int
+filter_w_ir_filter_imp_tick(
+    struct filter_w_ir_filter_imp *f_imp,
+    float *x,
+    float *y,
+    unsigned int len,
+    float *x_prev,
+    float *y_prev);
+
